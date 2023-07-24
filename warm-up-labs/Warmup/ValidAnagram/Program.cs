5fe1182135e9ccc3e4453bc030fe2a4367d58229
Glareone <enlarius@gmail.com>
2023-07-24 22:38:27 +0300

    valid anagram

---
+++ warm-up-labs/Warmup/ValidAnagram/Program.cs
@@ -0,0 +1,22 @@
+﻿using ValidAnagram;
+
+var word1 = "listen";
+var word2 = "silent";
+Console.WriteLine($"word1: {word1}, word2: {word2}");
+Console.WriteLine($"is valid anagram: {word1.isValidAnagramToWord(word2)}");
+
+word1 = "li2st1en ";
+word2 = "s8il en99t";
+Console.WriteLine($"word1: {word1}, word2: {word2}");
+Console.WriteLine($"is valid anagram: {word1.isValidAnagramToWord(word2)}");
+
+word1 = "rat";
+word2 = "car";
+Console.WriteLine($"word1: {word1}, word2: {word2}");
+Console.WriteLine($"is valid anagram: {word1.isValidAnagramToWord(word2)}");
+
+word1 = "hello";
+word2 = "ohel1o";
+Console.WriteLine($"word1: {word1}, word2: {word2}");
+Console.WriteLine($"is valid anagram: {word1.isValidAnagramToWord(word2)}");
+    
