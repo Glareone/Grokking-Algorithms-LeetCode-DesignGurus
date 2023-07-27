23293495475608173c374f4feb2b7a64346aee40
Glareone <enlarius@gmail.com>
2023-07-27 12:35:30 +0300

    added the second implementation which has better algorithm complexity

--- warm-up-labs/Warmup/WordDistance/Program.cs
+++ warm-up-labs/Warmup/WordDistance/Program.cs
@@ -1,4 +1,8 @@
-﻿var words = new [] { "the", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog" };
+﻿// Tests for the first implementation
+// Algorithm Complexity O(n2)
+// Space Complexity O(1)
+
+var words = new [] { "the", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog" };
 var word1 = "fox";
 var word2 = "dog";
 
@@ -25,3 +29,35 @@ word2 = "dog";
 
 Console.WriteLine(string.Join(',', words));
 Console.WriteLine($"distance between {word1} and {word2} is: {WordDistance.WordDistance.FindShortestDistanceForWords(words, word1, word2)}");
+
+// Tests for the second implementation
+// Algorithm Complexity O(n)
+// Space Complexity O(1)
+
+words = new [] { "the", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog" };
+word1 = "fox";
+word2 = "dog";
+
+Console.WriteLine(string.Join(',', words));
+Console.WriteLine($"distance between {word1} and {word2} is: {WordDistance.WordDistance.FindShortestWordDistance(words, word1, word2)}");
+
+words = new [] { "the", "quick", "brown", "fox", "fox", "over", "fox", "lazy", "dog" };
+word1 = "fox";
+word2 = "dog";
+
+Console.WriteLine(string.Join(',', words));
+Console.WriteLine($"distance between {word1} and {word2} is: {WordDistance.WordDistance.FindShortestWordDistance(words, word1, word2)}");
+
+words = new [] { "the", "quick", "dog", "fox", "fox", "over", "fox", "lazy", "dog" };
+word1 = "fox";
+word2 = "dog";
+
+Console.WriteLine(string.Join(',', words));
+Console.WriteLine($"distance between {word1} and {word2} is: {WordDistance.WordDistance.FindShortestWordDistance(words, word1, word2)}");
+
+words = new [] { "dog", "quick", "mouse", "fox", "dog", "over", "the", "lazy", "dog" };
+word1 = "fox";
+word2 = "dog";
+
+Console.WriteLine(string.Join(',', words));
+Console.WriteLine($"distance between {word1} and {word2} is: {WordDistance.WordDistance.FindShortestWordDistance(words, word1, word2)}");
--- warm-up-labs/Warmup/WordDistance/WordDistance.cs
+++ warm-up-labs/Warmup/WordDistance/WordDistance.cs
@@ -2,6 +2,8 @@ namespace WordDistance;
 
 internal static class WordDistance
 {
+    // Algorithm Complexity O(n2)
+    // Space Complexity O(1)
     public static int FindShortestDistanceForWords(this string[] initialWordArray, string word1, string word2)
     {
         var minDistance = initialWordArray.Length - 1;
@@ -34,6 +36,38 @@ internal static class WordDistance
             if (distance < minDistance)
             {
                 minDistance = distance.Value;
+            }
+        }
+
+        return minDistance;
+    } 
+    
+    // Algorithm Complexity O(n)
+    // Space Complexity O(1)
+    public static int FindShortestWordDistance(this string[] initialWordArray, string word1, string word2)
+    {
+        int? word1Pointer = null;
+        int? word2Pointer = null;
+        var minDistance = initialWordArray.Length - 1;
+
+        for (var i = 0; i <= initialWordArray.Length - 1; i++)
+        {
+            if (initialWordArray[i].Equals(word1))
+            {
+                word1Pointer = i;
+            }
+            else if(initialWordArray[i].Equals(word2))
+            {
+                word2Pointer = i;
+            }
+
+            if (word1Pointer.HasValue && word2Pointer.HasValue)
+            {
+                var distance = Math.Abs(word1Pointer.Value - word2Pointer.Value);
+                if (distance < minDistance)
+                {
+                    minDistance = distance;
+                }
             }
         }
 
