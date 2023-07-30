2b9ff00f5dbbdffe2ab703ea680ab41db5725c79
Glareone <enlarius@gmail.com>
2023-07-30 22:13:06 +0300

    bug fixing, code cleanup, add additional cycle and checks

--- fast-slow-pointers/FastSlowPointers/LinkedListCycle/Program.cs
+++ fast-slow-pointers/FastSlowPointers/LinkedListCycle/Program.cs
@@ -13,3 +13,10 @@ singleLinkedList.Traverse();
 singleLinkedList.CheckForCycle();
 singleLinkedList.AddCycleToHead();
 singleLinkedList.CheckForCycle();
+
+singleLinkedList = new SinglyLinkedList();
+singleLinkedList.Init();
+singleLinkedList.Traverse();
+singleLinkedList.CheckForCycle();
+singleLinkedList.AddCycleToRandomPlace();
+singleLinkedList.CheckForCycle();
