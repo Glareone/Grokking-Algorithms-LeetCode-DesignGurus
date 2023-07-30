1d2b74e85162a5bff2bea87922f84edaf0934d19
Glareone <enlarius@gmail.com>
2023-07-30 21:39:05 +0300

    fast-slow-pointers

---
+++ fast-slow-pointers/FastSlowPointers/LinkedListCycle/Program.cs
@@ -0,0 +1,15 @@
+﻿using LinkedListCycle;
+
+var singleLinkedList = new SinglyLinkedList();
+singleLinkedList.Init();
+singleLinkedList.Traverse();
+singleLinkedList.CheckForCycle();
+singleLinkedList.AddCycleToLast();
+singleLinkedList.CheckForCycle();
+
+singleLinkedList = new SinglyLinkedList();
+singleLinkedList.Init();
+singleLinkedList.Traverse();
+singleLinkedList.CheckForCycle();
+singleLinkedList.AddCycleToHead();
+singleLinkedList.CheckForCycle();
