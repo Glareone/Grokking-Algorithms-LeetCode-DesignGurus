548cbc2d832cc14a8ee982610e1a768dabdfab0c
Aleksey Kolesnikov <enlarius@gmail.com>
2024-01-02 11:49:03 +0200

    sum of two pointers

--- main/DesignGugus.Algorithms/DesignGugus.TwoPointers/Program.cs
+++ main/DesignGugus.Algorithms/DesignGugus.TwoPointers/Program.cs
@@ -1,3 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using DesignGugus.TwoPointers;
 
-Console.WriteLine("Hello, World!");
+var arrayForSumOfTwo = new int[] { 1, 2, 3, 4, 6 };
+
+var (leftIndex, rightIndex) = arrayForSumOfTwo.GetElementIndexesWithSum(6);
+Console.WriteLine($"[{leftIndex}, {rightIndex}]");
+
+arrayForSumOfTwo = new int[] { 2, 5, 9, 11 };
+var (leftIndex2, rightIndex2) = arrayForSumOfTwo.GetElementIndexesWithSum(11);
+Console.WriteLine($"[{leftIndex2}, {rightIndex2}]");
---
+++ main/DesignGugus.Algorithms/DesignGugus.TwoPointers/SumOfTwoInArray.cs
@@ -0,0 +1,28 @@
+namespace DesignGugus.TwoPointers;
+
+public static class SumOfTwoInArray
+{
+    public static (int leftIndex, int rightIndex) GetElementIndexesWithSum(this int[] arrayElements, int expectedSum)
+    {
+        if (arrayElements.Length <= 1)
+        {
+            return (0, 0);
+        }
+        
+        var leftPosition = 0;
+        var rightPosition = arrayElements.Length - 1;
+
+        while (arrayElements[leftPosition] + arrayElements[rightPosition] != expectedSum && leftPosition != rightPosition)
+        {
+            if (arrayElements[leftPosition] + arrayElements[rightPosition] < expectedSum)
+            {
+                leftPosition += 1;
+                continue;
+            }
+
+            rightPosition -= 1;
+        }
+
+        return (leftPosition, rightPosition);
+    }
+}
