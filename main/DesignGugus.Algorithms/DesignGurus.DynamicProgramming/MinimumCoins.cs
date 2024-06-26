f1ad6d51d1348202b5147d22630b6b64bb3062fb
Aleksey Kolesnikov <enlarius@gmail.com>
2024-06-26 09:20:42 +0300

    find number of coins with memoisaition

--- main/DesignGugus.Algorithms/DesignGurus.DynamicProgramming/MinimumCoins.cs
+++ main/DesignGugus.Algorithms/DesignGurus.DynamicProgramming/MinimumCoins.cs
@@ -2,6 +2,8 @@ namespace DesignGurus.DynamicProgramming;
 
 public class MinimumCoins
 {
+    private Dictionary<int, int> numberOfCoinsForTargetSum = new();
+    
     public static int? Min(int? a, int? b)
     {
         if (!a.HasValue) return b;
@@ -9,6 +11,43 @@ public class MinimumCoins
         return Math.Min(a.Value, b.Value);
     }
     
+    public int? FindMinimumCoinsNumberWithMemo(int targetSum, int[] availableCoins)
+    {
+        if (targetSum == 0) return 0;
+
+        if (numberOfCoinsForTargetSum.TryGetValue(targetSum, out var numberOfCoins))
+        {
+            return numberOfCoins;
+        }
+
+        int? answer = null;
+
+        foreach (var coin in availableCoins)
+        {
+            var subTotalSum = targetSum - coin;
+            if (subTotalSum < 0) continue;
+
+            var subResult = FindMinimumCoinsNumber(subTotalSum, availableCoins);
+            
+            // if sum total number of coins exist - we need to add 1 because
+            // we found the coin, need to find others
+            if (subResult.HasValue)
+            {
+                subResult += 1;
+            }
+            
+            answer = Min(answer, subResult);
+
+            if (answer.HasValue)
+            {
+                // coins for TargetSum found, memoize
+                numberOfCoinsForTargetSum.TryAdd(targetSum, answer.Value);
+            }
+        }
+
+        return answer;
+    }
+
     public static int? FindMinimumCoinsNumber(int targetSum, int[] availableCoins)
     {
         if (targetSum == 0) return 0;
--- main/DesignGugus.Algorithms/DesignGurus.DynamicProgramming/Program.cs
+++ main/DesignGugus.Algorithms/DesignGurus.DynamicProgramming/Program.cs
@@ -13,4 +13,10 @@ Console.WriteLine($"Fibonacci, bottom-up approach, result: {new FibonacciNumbers
 
 // Find Minimum number of coins
 // Alg Complexity O(N^2) because of recursion
+// Mem Complexity O(1)
 Console.WriteLine($"minimum number of coins needed to get the targetSum is {MinimumCoins.FindMinimumCoinsNumber(13, new []{ 1,4,5 })}");
+
+// Find minimum number of coins with memo
+// Alg Complexity O(N^2) still because of recursion
+// Memory Complexity O(N^2)
+Console.WriteLine($"minimum number of coins needed to get the targetSum is {new MinimumCoins().FindMinimumCoinsNumberWithMemo(13, new []{ 1,4,5 })}");
